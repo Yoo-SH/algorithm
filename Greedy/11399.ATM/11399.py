e5a4b29926f2b5314046f74f1d29729718e7a378
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-17 22:35:02 +0900

    feat:11399.ATM 문제 풀이 추가

---
+++ Greedy/11399.ATM/11399.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    n = int(input())
+    p = list(map(int, input().split()))
+    p.sort()
+    sum = 0
+    for i in range(n):
+        sum += p[i] * (n - i)
+    print(sum)
